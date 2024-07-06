a39198cf83601c2befcc4bab3f9df0be4d1064ec
h4ckerlite <70925909+h4ckerlite@users.noreply.github.com>
2024-07-06 18:47:10 -0400

    Parcial #2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('July 6, 2024 15:00:00').getTime();
+    const countDate = new Date('July 7, 2024 09:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
   
