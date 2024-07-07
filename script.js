5ceec99aeeeb2317de3324bd784324f5a9c81f53
h4ckerlite <70925909+h4ckerlite@users.noreply.github.com>
2024-07-07 19:09:07 -0400

    Segundo Parcial

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('July 7, 2024 09:00:00').getTime();
+    const countDate = new Date('July 28, 2024 09:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
   
